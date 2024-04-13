cda864d22d807c968039dbce02b471ec5e87ba8c
Niag Alves <niag.souza1993@hotmail.com>
2024-04-13 19:37:25 -0300

    funções recursivas

---
+++ 19-funcoes-recursivas/main.go
@@ -0,0 +1,19 @@
+// são funções que chamam ela mesma
+
+package main
+
+import "fmt"
+
+func fibonacci(posicao uint) uint {
+	if posicao <= 1 {
+		return posicao
+	}
+
+	return fibonacci(posicao-2) + fibonacci(posicao-1)
+
+}
+
+func main() {
+	posicao := uint(10)
+	fmt.Println(fibonacci(posicao))
+}
